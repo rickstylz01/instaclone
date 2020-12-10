666abc2f53362d1ffeac125e7bd701e325e3d084
Slick Rick <Rickystyles@BBOYs-iMac.local>
2020-12-10 14:43:37 -0600

    added new folder for main navbar componenet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Nav from './components/Nav'
+import MainNavbar from './components/MainNavbar/MainNavbar'
 
 function App() {
   return (
     <div>
-      <Nav />
+      <MainNavbar />
       <p>Hello, world!</p>
     </div>
   );
