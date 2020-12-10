60d87c645f8e64ef5b803002b32dd31b40efebe8
Slick Rick <Rickystyles@BBOYs-iMac.local>
2020-12-10 13:50:59 -0600

    add button for searching in the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 
 class Header extends React.Component {
   render() {
     return (
       <Nav className="justify-content-end" activeKey="/home">
+        <h2 id="navTitle">Rickstagram</h2>
+        <Button variant="outline-primary">Primary</Button>
         <Nav.Item>
           <Nav.Link href="/home">Active</Nav.Link>
         </Nav.Item>
