fb67b5a053ce1ef77d7ace4c44e0ec2fb87adb97
Slick Rick <Rickystyles@BBOYs-iMac.local>
2020-12-10 15:11:55 -0600

    add searchbar and styling to searchbar on nav

--- src/components/MainNavbar/MainNavbar.js
+++ src/components/MainNavbar/MainNavbar.js
@@ -1,28 +1,44 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from 'react-bootstrap/FormControl';
 
 class Header extends React.Component {
   render() {
+    //inputgroup = navbar
     return (
-      <Nav className="justify-content-end" activeKey="/home">
-        <h2 id="navTitle">Rickstagram</h2>
-        <Button variant="outline-primary">Primary</Button>
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <div>
+        <Nav className="justify-content-end" activeKey="/home">
+          <h2 id="navTitle">Rickstagram</h2>
+          <div id="searchBar">
+            <InputGroup size="sm" className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Small"
+              aria-describedby="inputGroup-sizing-sm"
+            />
+          </InputGroup>
+        </div>
+          <Nav.Item>
+            <Nav.Link href="/home">
+              Active
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
     );
   }
 }
