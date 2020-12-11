17e405fbd0bb3ea3b7388844b704627d3b376678
Rick Maya <rickmaya@gmail.com>
2020-12-11 14:06:27 -0600

    changed styling of navbar component

--- src/components/MainNavbar/MainNavbar.js
+++ src/components/MainNavbar/MainNavbar.js
@@ -1,61 +1,42 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
-import InputGroup from "react-bootstrap/InputGroup";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl";
 
 class Header extends React.Component {
   render() {
     //inputgroup = navbar
     return (
-      <div>
-        <Nav className="justify-content-end" activeKey="/home">
-          <h2 id="navTitle">Rickstagram</h2>
-          
-          <div id="searchBar">
-            <InputGroup size="sm" className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-sm">
-                  Search
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                aria-label="Small"
-                aria-describedby="inputGroup-sizing-sm"
-              />
-            </InputGroup>
-          </div>
-          
-          <Nav.Item>
+      <div id="MainNavbar">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-primary">Search</Button>
+          </Form>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
             <svg
               width="2em"
               height="2em"
               viewBox="0 0 16 16"
-              class="bi bi-house-fill"
+              className="bi bi-house-fill"
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
               />
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
               />
-            </svg>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
+            </svg></Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
