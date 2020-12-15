f93d379b3a2e820734b74c819fab220b46361b95
Rick Maya <rickmaya@gmail.com>
2020-12-15 13:53:07 -0600

    add image to card body and use CardDeck to create multiple card posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import MainNavbar from './components/MainNavbar/MainNavbar'
-import PostCard from './components/PostCard/PostCard'
+import { CardDeck, Container } from 'react-bootstrap';
+import MainNavbar from './components/MainNavbar/MainNavbar';
+import PostCard from './components/PostCard/PostCard';
 
 function App() {
   return (
     <div>
       <MainNavbar />
-      <PostCard />
+      <Container>
+        <CardDeck className="mt-3">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </CardDeck>
+      </Container> 
     </div>
   );
 }
