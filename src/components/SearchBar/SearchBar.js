dfdbcf31bb7c12357770cf08054b8c47ac58bac9
Rick Maya <rickmaya@gmail.com>
2021-01-07 12:54:03 -0600

    refactored SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,47 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl";
 
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-    };
-  }
+function SearchBar(props) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     searchTerm: '',
+  //   };
+  // }
 
-  editSearchTerm = (query) => {
-    this.setState({ searchTerm: query.target.value })
-  }
+  // editSearchTerm = (query) => {
+  //   this.setState({ searchTerm: query.target.value })
+  // }
 
-  dynamicSearch = () => {
-    return this.props.data.filter(
-      postObject => postObject.username.toLocaleLowerCase().includes(
-        this.state.searchTerm.toLocaleLowerCase()))
-  }
+  // dynamicSearch = () => {
+  //   return this.props.data.filter(
+  //     postObject => postObject.username.toLocaleLowerCase().includes(
+  //       this.state.searchTerm.toLocaleLowerCase()))
+  // }
+  const { searchTerm, setSearchTerm } = props
 
-  render() {
-    return (
-      <div>
-        <Form className="ml-auto" inline>
-          <FormControl 
-          type="text" 
-          placeholder="Search" 
-          className="mr-sm-2"
-          value={this.state.searchTerm}
-          onChange={this.editSearchTerm} />
+  return (
+    <div>
+      <Form className="ml-auto" inline>
+        <FormControl 
+        type="text" 
+        placeholder="Search" 
+        className="mr-sm-2"
+        value={searchTerm}
+        onChange={(query) => setSearchTerm(query.target.value)} />
 
-          <Button 
-          variant="outline-success">
-            Search
-          </Button>
-        </Form>
-        
-      </div>
-    );
-  }
+        <Button 
+        variant="outline-success">
+          Search
+        </Button>
+      </Form>
+      
+    </div>
+  );
+  
 }
 
 export default SearchBar
